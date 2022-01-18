bb995ff1d2300eef953ecb31b11da69f10e3eff1
Antonio Carlos Pinheiro <prog.antoniocarlos@outlook.com>
2022-01-18 13:29:42 -0300

    Inicial

---
+++ Criptografia2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+
+namespace Criptografia2
+{
+    internal class Program
+    {
+
+
+        // http://msdn.microsoft.com/en-us/library/system.security.cryptography.symmetricalgorithm.aspx
+        static void Main(string[] args)
+        {
+            string textoOutput = "antonio";
+
+            var criptografado = Cryptography.Encrypt<RijndaelManaged>(textoOutput, "123456");
+            var original = Cryptography.Decrypt<RijndaelManaged>(criptografado, "123458989");
+
+            var criptografado2 = Cryptography.Encrypt<RijndaelManaged>(textoOutput, "123456");
+            var original2 = Cryptography.Decrypt<RijndaelManaged>(criptografado, "123456");
+
+            if (string.Equals(textoOutput, original))
+            {
+                Console.WriteLine("funcionou");
+            };
+        }
+    }
+}
